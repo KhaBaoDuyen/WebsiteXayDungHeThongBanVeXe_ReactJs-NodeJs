4424f271e9c462fafad423662df263e57fdd6663
KhaBaoDuyen <khathibaoduyen9@gmail.com>
2025-03-13 14:13:32 +0700

    [ FE ] - cài taiwwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./FE/src/**/*.{html,js,jsx,ts,tsx}"], // Đường dẫn file cần quét
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
