0d99b7d9aab51b80a8025c6a3244e376489dd87e
KhaBaoDuyen <khathibaoduyen9@gmail.com>
2025-03-13 20:56:21 +0700

    [ FE ] - header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./FE/src/**/*.{html,js,jsx,ts,tsx}"], // Quét toàn bộ file trong thư mục FE/src/
+  content: ["./FE/src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
